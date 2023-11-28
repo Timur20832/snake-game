6fc24acbb20ba98d1534b76c92a42c197502fd9c
Timur20832 <92998774+Timur20832@users.noreply.github.com>
2023-11-28 22:13:42 +0300

    game

---
+++ змейка/змейка/snakeGame.cpp
@@ -0,0 +1,223 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <iterator>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <random>
+
+using namespace std;
+
+void gotoxy(int x, int y) {
+    COORD pos = { x, y };
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(output, pos);
+}
+
+void HandleInput(int& direction, const int UP, const int DOWN, const int RIGHT, const int LEFT) {
+    if (_kbhit()) {
+        char key = _getch();
+        switch (key) {
+        case 'w':
+            if (direction != DOWN) direction = UP;
+            break;
+        case 's':
+            if (direction != UP) direction = DOWN;
+            break;
+        case 'a':
+            if (direction != RIGHT) direction = LEFT;
+            break;
+        case 'd':
+            if (direction != LEFT) direction = RIGHT;
+            break;
+        }
+    }
+}
+
+void PrintField(const vector<vector<char>>& field, const int HEIGHT, const int WIDTH) {
+    for (int i = 0; i < HEIGHT; i++) {
+        copy(field[i].begin(), field[i].begin() + WIDTH, ostream_iterator<char>(cout, " "));
+        cout << endl;
+    }
+}
+
+void FillField(vector<vector<char>>& field, const int HEIGHT, const int WIDTH) {
+    for (int i = 0; i < HEIGHT; i++) {
+        vector<char> tempVector(WIDTH);
+        for (int j = 0; j < WIDTH; j++) {
+            tempVector[j] = '.';
+        }
+        field[i] = tempVector;
+    }
+}
+
+struct Position {
+    int X;
+    int Y;
+};
+
+bool CheckSelfCollision(const deque<Position>& snake) {
+    Position head = snake.front();
+    for (auto it = snake.begin() + 1; it != snake.end(); ++it) {
+        if (head.X == it->X && head.Y == it->Y) {
+            return true; // Столкновение с хвостом
+        }
+    }
+    return false; // Нет столкновения с хвостом
+}
+
+bool CheckApplePos(const deque<Position>& snake, const Position apple)
+{
+    for (const auto& segment : snake)
+    {
+        if (apple.X == segment.X && apple.Y == segment.Y)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+Position GenerateApplePosition(const deque<Position>& snake, const int FIELD_WIDTH, const int FIELD_HEIGHT) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> widthDistribution(0, FIELD_WIDTH - 1);
+    uniform_int_distribution<int> heightDistribution(0, FIELD_HEIGHT - 1);
+
+    Position applePos;
+    bool isOnSnake;
+    do {
+        applePos.X = widthDistribution(gen);
+        applePos.Y = heightDistribution(gen);
+
+        isOnSnake = false;
+        for (const auto& segment : snake) {
+            if (applePos.X == segment.X && applePos.Y == segment.Y) {
+                isOnSnake = true;
+                break;
+            }
+        }
+    } while (isOnSnake);
+    return applePos;
+}
+
+int main() {
+    setlocale(0, "");
+    srand(time(NULL));
+    ShowCursor(FALSE);
+
+    const int FIELD_HEIGHT = 4;
+    const int FIELD_WIDTH = 4;
+
+    const int MAX_SNAKE_LENGTH = FIELD_HEIGHT * FIELD_WIDTH;
+
+    const int DOWN = 0;
+    const int UP = 1;
+    const int LEFT = 2;
+    const int RIGHT = 3;
+
+    int direction = UP;
+
+    bool isContinue = true;
+
+    vector <vector<char>> gameField(FIELD_HEIGHT, vector<char>(FIELD_WIDTH));
+    int snakeLength = 1;
+    deque<Position> snake;
+    char apple = '+';
+
+    FillField(gameField, FIELD_HEIGHT, FIELD_WIDTH);
+
+    Position snakePos;
+    snakePos.X = FIELD_WIDTH / 2;
+    snakePos.Y = FIELD_HEIGHT / 2;
+
+    snake.push_back(snakePos);
+
+    Position applePos = GenerateApplePosition(snake, FIELD_WIDTH, FIELD_HEIGHT);
+
+    while (isContinue) {
+        gotoxy(0, 0);
+
+        cout << "Ваш счёт: " << snakeLength << endl;
+
+        // Очистка только тех мест, где были элементы (тело змейки и яблоко)
+        gameField[applePos.Y][applePos.X] = '.';
+        for (const auto& segment : snake) {
+            gameField[segment.Y][segment.X] = '.';
+        }
+
+        HandleInput(direction, UP, DOWN, RIGHT, LEFT);
+
+        // Обновление позиции змейки в зависимости от направления
+        Position newHead = snake.front();
+        switch (direction) {
+        case UP:
+            newHead.Y--;
+            break;
+        case DOWN:
+            newHead.Y++;
+            break;
+        case LEFT:
+            newHead.X--;
+            break;
+        case RIGHT:
+            newHead.X++;
+            break;
+        }
+
+        if (snakeLength == MAX_SNAKE_LENGTH)
+        {
+            isContinue = false;
+            gotoxy(2, FIELD_HEIGHT / 2);
+            cout << "ВЫ ПОБЕДИЛИ!" << endl;
+            gotoxy(0, FIELD_HEIGHT);
+            break;
+        }
+
+        // Проверка на столкновение с границами поля
+        if (newHead.X < 0 || newHead.X >= FIELD_WIDTH || newHead.Y < 0 || newHead.Y >= FIELD_HEIGHT) {
+            isContinue = false;
+            gotoxy(2, FIELD_HEIGHT / 2);
+            cout << "Конец игры! Ваш счет: " << snakeLength << endl;
+            gotoxy(0, FIELD_HEIGHT);
+            break;
+            // Игра завершается при столкновении со стеной
+        }
+
+        // Проверка на столкновение с хвостом
+        if (CheckSelfCollision(snake)) {
+            isContinue = false;
+            gotoxy(2, FIELD_HEIGHT / 2);
+            cout << "Конец игры! Столкновение с хвостом. Ваш счет: " << snakeLength << endl;
+            gotoxy(0, FIELD_HEIGHT);
+            break;
+        }
+
+
+        if (newHead.X == applePos.X && newHead.Y == applePos.Y) {
+            applePos = GenerateApplePosition(snake, FIELD_WIDTH, FIELD_HEIGHT);
+            snakeLength++;
+            gameField[applePos.Y][applePos.X] = apple;
+        }
+
+        snake.push_front(newHead);
+        for (const auto& segment : snake) {
+            gameField[segment.Y][segment.X] = 'O';
+        }
+
+        // Удаление хвоста змейки, если её длина превышает максимальное значение
+        if (snake.size() > snakeLength) {
+            gameField[snake.back().Y][snake.back().X] = '.';
+            snake.pop_back();
+        }
+
+        gameField[applePos.Y][applePos.X] = apple;
+
+        PrintField(gameField, FIELD_HEIGHT, FIELD_WIDTH);
+
+        Sleep(500);
+    }
+    system("pause");
+    return 0;
+}
---
+++ змейка/змейка/змейка.cpp
@@ -0,0 +1,239 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <conio.h>
+#include <windows.h>
+#include <algorithm>
+#include <random>
+#include <utility>
+
+using namespace std;
+
+struct Cordinate {
+	int x;
+	int y;
+};
+
+const int fieldWidth = 22;
+const int fieldHeidth = 22;
+const int maxLengthSnake = (fieldWidth - 2) * (fieldHeidth - 2);
+
+int score;
+bool newFruit = true;
+Cordinate fruit;
+deque<Cordinate> snake;
+vector<vector<char>> gameField(fieldHeidth, vector<char>(fieldWidth));
+enum Direction { Stop = 0, Left, Right, Up, Down };
+Direction dir;
+
+void GoToXY(int x, int y) {
+	COORD pos = { x,y };
+	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(output, pos);
+}
+
+Cordinate RandomAvaibleCoordinate(vector<Cordinate> Coordinate) {
+	random_device rd;
+	mt19937 rng(rd());
+	shuffle(Coordinate.begin(), Coordinate.end(), rng);
+	Cordinate randomCoordinate = Coordinate[0];
+	return randomCoordinate;
+}
+
+void ChooseDiraction(bool GameOver) {
+	if (_kbhit()) {
+		switch (_getch())
+		{
+		case ('a'):
+			if (dir != Right) dir = Left;
+			break;
+		case ('w'):
+			if (dir != Down) dir = Up;
+			break;
+		case ('d'):
+			if (dir != Left) dir = Right;
+			break;
+		case ('s'):
+			if (dir != Up) dir = Down;
+			break;
+		case ('x'):
+			GameOver = true;
+			break;
+		}
+	}
+}
+
+Cordinate ChangePositionSnakeHead(Cordinate snakeHead) {
+	switch (dir)
+	{
+	case Left:
+		snakeHead.x--;
+		break;
+	case Right:
+		snakeHead.x++;
+		break;
+	case Up:
+		snakeHead.y--;
+		break;
+	case Down:
+		snakeHead.y++;
+		break;
+	}
+	return snakeHead;
+}
+
+Cordinate SnakeTeleport(Cordinate Head) {
+	if (Head.x >= fieldWidth - 1) Head.x = 1;
+	else if (Head.y >= fieldHeidth - 1) Head.y = 1;
+	else if (Head.y == 0) Head.y = fieldHeidth - 2;
+	else if (Head.x == 0) Head.x = fieldWidth - 2;
+	return Head;
+}
+
+bool WinGame(int snakeLength) {
+	if (snakeLength >= maxLengthSnake) return true;
+	return false;
+}
+
+bool LoseGame(Cordinate Head) {
+	for (int i = 1; i < snake.size(); i++) {
+		if (Head.x == snake[i].x && Head.y == snake[i].y) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void SnakeMove(Cordinate Last) {
+	for (int i = 1; i < snake.size(); i++) {
+		Cordinate temp = snake[i];
+		snake[i] = Last;
+		Last = temp;
+	};
+}
+
+bool CheckingPosition(int y, int x, deque<Cordinate> AllCheck) {
+	deque<Cordinate> temp = AllCheck;
+	while (!temp.empty()) {
+		Cordinate CurrentElem = temp.front();
+		if (CurrentElem.x == x && CurrentElem.y == y) return true;
+		temp.pop_front();
+	}
+	return false;
+}
+
+void DrawSnake() {
+	for (int i = 0; i < snake.size(); i++) {
+		if (i == 0) {
+			gameField[snake[i].y][snake[i].x] = '&';
+		}
+		else {
+			gameField[snake[i].y][snake[i].x] = 'o';
+		}
+	}
+}
+
+void DrawFruit(vector<Cordinate> AvailableCoordinates) {
+	Cordinate CurrentHead = snake.front();
+	if (CurrentHead.x == fruit.x && CurrentHead.y == fruit.y) {
+		newFruit = true;
+	}
+	//генерация нового яблока
+	if (newFruit) {
+		fruit = RandomAvaibleCoordinate(AvailableCoordinates);
+		gameField[fruit.y][fruit.x] = '$';
+		AvailableCoordinates.clear();
+		newFruit = false;
+	}
+	else {
+		gameField[fruit.y][fruit.x] = '$';
+	}
+}
+
+void Instalisation(bool gameOver) {
+	gameOver = false;
+	snake.push_front({ fieldWidth / 2, fieldHeidth / 2 });
+	snake.push_back({ fieldWidth / 2, fieldHeidth / 2 + 1 });
+	dir = Up;
+}
+
+bool Logic(bool gameOver) {
+	ChooseDiraction(gameOver);
+	Cordinate head = snake.front();
+	Cordinate last = head;
+	Cordinate Back_tail = snake.back();
+
+	head = ChangePositionSnakeHead(head);
+	if (head.x == fruit.x && head.y == fruit.y) {
+		score++;
+		snake.push_back(Back_tail);
+	}
+
+	head = SnakeTeleport(head);
+
+	gameOver = LoseGame(head);
+	if (!gameOver) gameOver = WinGame(score + 2);
+
+	for (int i = 1; i < snake.size(); i++) {
+		Cordinate temp = snake[i];
+		snake[i] = last;
+		last = temp;
+	};
+
+	snake.pop_front();
+	snake.push_front(head);
+	return gameOver;
+}
+
+
+void CreateField() {
+	vector<Cordinate> AvailableCoordinates;
+	for (int i = 0; i < gameField.size(); i++) {
+		for (int j = 0; j < gameField[i].size(); j++) {
+			if (CheckingPosition(i, j, snake)) {
+				gameField[i][j] = ' ';
+			}
+			if ((i == 0) || (i + 1 == fieldHeidth)) {
+				gameField[i][j] = '#';
+			}
+			else if ((j == 0) || (j + 1 == fieldWidth)) {
+				gameField[i][j] = '#';
+			}
+			else {
+				gameField[i][j] = ' ';
+				AvailableCoordinates.push_back({ j, i });
+			}
+		}
+	}
+	DrawSnake();
+	DrawFruit(AvailableCoordinates);
+}
+
+void PrintField() {
+	GoToXY(0, 0);
+	cout << "Score: " << score << endl;
+	CreateField();
+	for (int i = 0; i < gameField.size(); i++) {
+		for (int j = 0; j < gameField[i].size(); j++) {
+			cout << gameField[i][j];
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	bool gameOver = false;
+	Instalisation(gameOver);
+	int time = clock();
+	while (!gameOver)
+	{
+		gameOver = Logic(gameOver);
+		PrintField();
+		Sleep(300);
+	}
+	system("cls");
+	cout << "Game over" << endl;
+	cout << "Your Score: " << score;
+	return 0;
+}
+
